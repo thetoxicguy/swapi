09253afcfa339e28bc55b50f49680c1b098747a9
danrolo <darobles@ciencias.unam.mx>
2024-05-17 13:22:37 -0600

    feat (app): integration with navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,56 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import People from '../screens/People';
 import Spaceships from '../screens/Spaceships';
 import Planets from '../screens/Planets';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
     <Tab.Navigator initialRouteName='People'>
-      <Tab.Screen name='People' component={People} />
-      <Tab.Screen name='Spaceships' component={Spaceships} />
-      <Tab.Screen name='Planets' component={Planets} />
+      <Tab.Screen
+        name='People'
+        component={People}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name='human' color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Spaceships'
+        component={Spaceships}
+        options={{
+          tabBarLabel: 'Spaceships',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name='rocket-launch'
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Planets'
+        component={Planets}
+        options={{
+          tabBarLabel: 'Planets',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name='google-earth'
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
