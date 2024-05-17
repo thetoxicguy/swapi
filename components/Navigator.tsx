9350ed9a6c3fc59765e30200345ceb850e863800
danrolo <darobles@ciencias.unam.mx>
2024-05-17 13:46:37 -0600

    feat (Table): Basic component created with RNP

---
+++ components/Navigator.tsx
@@ -0,0 +1,56 @@
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import People from '../screens/People';
+import Spaceships from '../screens/Spaceships';
+import Planets from '../screens/Planets';
+
+const Tab = createMaterialBottomTabNavigator();
+
+const Navigator = () => {
+  return (
+    <Tab.Navigator initialRouteName='People'>
+      <Tab.Screen
+        name='People'
+        component={People}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name='human' color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Spaceships'
+        component={Spaceships}
+        options={{
+          tabBarLabel: 'Spaceships',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name='rocket-launch'
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Planets'
+        component={Planets}
+        options={{
+          tabBarLabel: 'Planets',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name='google-earth'
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default Navigator;
--- screens/People.tsx
+++ screens/People.tsx
@@ -1,9 +1,11 @@
+import Table from '@/components/Table';
 import { View, Text } from 'react-native';
 
 const People = () => {
   return (
     <View>
       <Text>People</Text>
+      <Table />
     </View>
   );
 };
